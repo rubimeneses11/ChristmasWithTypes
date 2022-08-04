a9e965cfaed84f2dc299ac5adb3359562cfd2293
Rubi Meneses <rubi.ventura@icloud.com>
2022-08-04 13:53:28 -0500

    completed tree and day properties

--- ChristmasWithTypes/Christmas.cs
+++ ChristmasWithTypes/Christmas.cs
@@ -7,8 +7,8 @@ namespace ChristmasWithTypes
         public string Santa { get; set; }
         public int TreeHeight { get; set; } //TODO Make the Height property nullable
 
-        //TODO Make the property, "Day", type enum
-        public enum Day { /*Fill out the days of the week*/ };
+        //DONE Make the property, "Day", type enum
+        public enum Day { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
 
     }
 }
